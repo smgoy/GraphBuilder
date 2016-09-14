92dc480215fe5312abdf5bfa917a9591dcb69a57
smgoy <smgoy31@gmail.com>
2016-09-14 13:50:42 -0700

    renders links on screen

--- js/force_layout.jsx
+++ js/force_layout.jsx
@@ -13,19 +13,34 @@ const data = {
   ]
 };
 
+const radius = 16;
+
 class ForceLayout extends React.Component {
 
   componentDidMount() {
     const { width, height } = this.props;
+    const center = [width/2, height / 2];
 
     const simulation = d3.forceSimulation(data.nodes)
-      .force("charge", d3.forceManyBody().strength(-500))
-      .force("link", d3.forceLink(data.links).distance(150));
+      .force("charge", d3.forceManyBody().strength(-300))
+      .force("link", d3.forceLink(data.links).distance(150))
+      .force("center", d3.forceCenter(center[0], center[1]));
 
-    this.placeSVG(width, height);
 
-    simulation.on("tick", (e) => {
+    const svg = this.placeSVG(width, height);
+    const link = this.placeLinks(svg);
+    const node = this.placeNodes(svg);
 
+    simulation.on("tick", () => {
+      link
+        .attr('x1', (d) => d.source.x)
+        .attr('y1', (d) => d.source.y)
+        .attr('x2', (d) => d.target.x)
+        .attr('y2', (d) => d.target.y);
+
+      node
+        .attr('cx', (d) => d.x)
+        .attr('cy', (d) => d.y);
     });
 
   }
@@ -35,6 +50,29 @@ class ForceLayout extends React.Component {
       .append('svg')
       .attr('width', width)
       .attr('height', height);
+
+    return svg;
+  }
+
+  placeNodes(svg) {
+    const node = svg.selectAll('circle')
+      .data(data.nodes)
+      .enter()
+      .append('circle')
+      .attr('r', radius)
+      .classed('node', true);
+
+    return node;
+  }
+
+  placeLinks(svg) {
+    const link = svg.selectAll('line')
+      .data(data.links)
+      .enter()
+      .append('line')
+      .classed('link', true);
+
+    return link;
   }
 
   render() {
