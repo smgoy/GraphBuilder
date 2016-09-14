1e704112df084e76685f43eaa06064cabca0c0ed
smgoy <smgoy31@gmail.com>
2016-09-14 14:30:51 -0700

    adds functionality to drag nodes

--- js/force_layout.jsx
+++ js/force_layout.jsx
@@ -21,17 +21,16 @@ class ForceLayout extends React.Component {
     const { width, height } = this.props;
     const center = [width/2, height / 2];
 
-    const simulation = d3.forceSimulation(data.nodes)
+    this.simulation = d3.forceSimulation(data.nodes)
       .force("charge", d3.forceManyBody().strength(-300))
-      .force("link", d3.forceLink(data.links).distance(150))
+      .force("link", d3.forceLink(data.links).strength(1).distance(150))
       .force("center", d3.forceCenter(center[0], center[1]));
-
 
     const svg = this.placeSVG(width, height);
     const link = this.placeLinks(svg);
     const node = this.placeNodes(svg);
 
-    simulation.on("tick", () => {
+    this.simulation.on("tick", () => {
       link
         .attr('x1', (d) => d.source.x)
         .attr('y1', (d) => d.source.y)
@@ -60,9 +59,30 @@ class ForceLayout extends React.Component {
       .enter()
       .append('circle')
       .attr('r', radius)
-      .classed('node', true);
+      .classed('node', true)
+      .call(d3.drag()
+        .on("start", this.dragStarted.bind(this))
+        .on("drag", this.dragged.bind(this))
+        .on("end", this.dragEnded.bind(this)));
 
     return node;
+  }
+
+  dragStarted(d) {
+    if (!d3.event.active) this.simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+  }
+
+  dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+  }
+
+  dragEnded(d) {
+    if (!d3.event.active) this.simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
   }
 
   placeLinks(svg) {
