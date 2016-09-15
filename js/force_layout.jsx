0c0931cffdab6ec4c7701f6969f1c7c66a80abd9
smgoy <smgoy31@gmail.com>
2016-09-14 17:44:01 -0700

    saves work while to add node

--- js/force_layout.jsx
+++ js/force_layout.jsx
@@ -16,28 +16,35 @@ const data = {
 const radius = 16;
 
 class ForceLayout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      i: data.nodes.length,
+      data: data
+    };
+  }
 
   componentDidMount() {
     const { width, height } = this.props;
     const center = [width/2, height / 2];
 
-    this.simulation = d3.forceSimulation(data.nodes)
+    this.simulation = d3.forceSimulation(this.state.data.nodes)
       .force("charge", d3.forceManyBody().strength(-300))
-      .force("link", d3.forceLink(data.links).strength(1).distance(150))
+      .force("link", d3.forceLink(this.state.data.links).strength(.5).distance(150))
       .force("center", d3.forceCenter(center[0], center[1]));
 
-    const svg = this.placeSVG(width, height);
-    const link = this.placeLinks(svg);
-    const node = this.placeNodes(svg);
+    this.placeSVG(width, height);
+    this.placeLinks();
+    this.placeNodes();
 
     this.simulation.on("tick", () => {
-      link
+      this.link
         .attr('x1', (d) => d.source.x)
         .attr('y1', (d) => d.source.y)
         .attr('x2', (d) => d.target.x)
         .attr('y2', (d) => d.target.y);
 
-      node
+      this.node
         .attr('cx', (d) => d.x)
         .attr('cy', (d) => d.y);
     });
@@ -45,27 +52,34 @@ class ForceLayout extends React.Component {
   }
 
   placeSVG(width, height) {
-    const svg = d3.select(this.refs.mountPoint)
+    this.svg = d3.select(this.refs.mountPoint)
       .append('svg')
       .attr('width', width)
-      .attr('height', height);
-
-    return svg;
+      .attr('height', height)
+      .on('mousedown', this.addNode.bind(this));
   }
 
-  placeNodes(svg) {
-    const node = svg.selectAll('circle')
-      .data(data.nodes)
+  placeNodes() {
+    this.node = this.svg.selectAll('circle')
+      .data(this.state.data.nodes)
       .enter()
       .append('circle')
       .attr('r', radius)
+      .attr('stroke-width', 1.5)
       .classed('node', true)
+      .on('mousedown', this.nodeMouseDown)
       .call(d3.drag()
         .on("start", this.dragStarted.bind(this))
-        .on("drag", this.dragged.bind(this))
+        .on("drag", this.dragged)
         .on("end", this.dragEnded.bind(this)));
+  }
 
-    return node;
+  placeLinks() {
+    this.link = this.svg.selectAll('line')
+      .data(this.state.data.links)
+      .enter()
+      .append('line')
+      .classed('link', true);
   }
 
   dragStarted(d) {
@@ -85,14 +99,59 @@ class ForceLayout extends React.Component {
     d.fy = null;
   }
 
-  placeLinks(svg) {
-    const link = svg.selectAll('line')
-      .data(data.links)
-      .enter()
-      .append('line')
-      .classed('link', true);
+  addNode() {
+    this.simulation.stop();
+    this.state.data.nodes.push({index: this.state.i++});
 
-    return link;
+    const { width, height } = this.props;
+    const center = [width/2, height / 2];
+
+    this.simulation = d3.forceSimulation(this.state.data.nodes)
+      .force("charge", d3.forceManyBody().strength(-300))
+      .force("link", d3.forceLink(this.state.data.links).strength(.5).distance(150))
+      .force("center", d3.forceCenter(center[0], center[1]));
+
+      this.placeSVG(width, height);
+      this.placeLinks();
+      this.placeNodes();
+
+      this.simulation.on("tick", () => {
+        this.link
+          .attr('x1', (d) => d.source.x)
+          .attr('y1', (d) => d.source.y)
+          .attr('x2', (d) => d.target.x)
+          .attr('y2', (d) => d.target.y);
+
+        this.node
+          .attr('cx', (d) => d.x)
+          .attr('cy', (d) => d.y);
+      });
+  }
+
+  nodeMouseDown() {
+    d3.event.stopPropagation();
+
+    d3.selectAll('.selected').transition()
+      .duration(500)
+      .attr('stroke-width', 1.5);
+
+    const node = d3.select(this);
+
+    node.classed('selected', true);
+    node.transition()
+      .duration(100)
+      .attr('r', 19)
+      .transition()
+      .duration(100)
+      .attr('r', 16)
+      .duration(300)
+      .attr('stroke-width', 3);
+  }
+
+  deselectAll() {
+    this.nodes.transition()
+      .duration(100)
+      .attr('r', 16);
   }
 
   render() {
